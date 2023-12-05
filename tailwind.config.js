6634cfcba8b7bcc6a36b05827d9e9f97dfd20550
mqkhan <qasim_ajk@yahoo.com>
2023-12-05 19:35:31 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,13 @@ export default {
         },
         liColors: "#6b7280",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
     },
   },
   plugins: [],
